3d638c5eae6eda7fb20d4eca4288a7b025474ee4
Joe Dillon <joe7dillon@gmail.com>
2019-12-24 12:38:13 +0000

    Invalid /etc/taps.conf config formats will now alert the user and fallback to the template config file.

--- taps/__init__.py
+++ taps/__init__.py
@@ -11,8 +11,16 @@ PKG_DATA_CONF = pkg_resources.resource_filename("taps", "taps.conf")
 
 if os.path.exists(USER_CONFIG):
     user_config.read(USER_CONFIG)
+    # Check /etc/taps.conf config is correct.
+    for setting in ["Color", "White", "Amber", "Green", "Red"]:
+        if setting not in user_config["COLORS"]:
+            print("Invalid configuration file format. Defaulting to template.")
+            user_config.read(PKG_DATA_CONF)
+            break
 elif os.path.exists(PKG_DATA_CONF):
     user_config.read(PKG_DATA_CONF)
 else:
     print("Fatal: Template config file not found at install location.")
     exit()
+
+
